b0265f6681b391159ffa2e144ea949ca2df3661b
Aaro <aaro.lehtoaho@student.oulu.fi>
2025-04-22 12:26:57 +0300

    Added function to lift servos slowly

--- code/main.py
+++ code/main.py
@@ -1,9 +1,10 @@
 from time import sleep
-from motor import test_motors
+from motor import test_motors, stop
 from servos import lift
 
+stop()
+
 if __name__ == "__main__":
-    test_motors()
-    lift()
+    #test_motors()
     sleep(1)
     lift()
--- code/servos.py
+++ code/servos.py
@@ -1,8 +1,8 @@
 from machine import Pin, PWM
 from time import sleep
 
-DOWN_A, UP_A = 130, 0
-DOWN_B, UP_B = 30, 160
+DOWN_A, UP_A = 110, 0
+DOWN_B, UP_B = 50, 160
 
 servo_A = PWM(Pin(8))
 servo_A.freq(50)
@@ -15,28 +15,55 @@ def init_servos():
     servo_B = PWM(Pin(9))
     servo_B.freq(50)
 
-def set_angle_A(angle):
+def get_duty(angle):
     min_duty = 1500
     max_duty = 8000
     duty = int((angle / 180) * (max_duty - min_duty) + min_duty)
+    return duty
+
+def set_angle_a(angle):
+    duty = get_duty(angle)
     servo_A.duty_u16(duty)
 
-def set_angle_B(angle):
-    min_duty = 1500
-    max_duty = 8000
-    duty = int((angle / 180) * (max_duty - min_duty) + min_duty)
+def set_angle_b(angle):
+    duty = get_duty(angle)
     servo_B.duty_u16(duty)
 
+def fast_lift():
+    init_servos()
+    set_angle_b(UP_B)
+    set_angle_a(UP_A)
+    sleep(1)
+    set_angle_b(DOWN_B)
+    set_angle_a(DOWN_A)
+    sleep(0.5)
+    servo_A.deinit()
+    servo_B.deinit()
+    
 def lift():
     init_servos()
-    set_angle_B(UP_B)
-    set_angle_A(UP_A)
-    sleep(1)
-    set_angle_B(DOWN_B)
-    set_angle_A(DOWN_A)
+    
+    duty_a_down = get_duty(DOWN_A)
+    duty_a_up = get_duty(UP_A)
+    lift_range_a = range(duty_a_down, duty_a_up, -2)
+    
+    duty_b_down = get_duty(DOWN_B)
+    duty_b_up = get_duty(UP_B)
+    lift_range_b = range(duty_b_down, duty_b_up, 2)
+    
+    for a, b in zip(lift_range_a, lift_range_b):
+        servo_A.duty_u16(a)
+        servo_B.duty_u16(b)
+        sleep(0.001)
     sleep(0.5)
+    for a, b in zip(reversed(lift_range_a), reversed(lift_range_b)):
+        servo_A.duty_u16(a)
+        servo_B.duty_u16(b)
+        sleep(0.001)
+    sleep(0.1)
     servo_A.deinit()
     servo_B.deinit()
 
 if __name__ == "__main__":
+    sleep(2)
     lift()
